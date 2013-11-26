dea4d9dab10887853dd1a1ea5324073c2f8850c2
MichaelQTLam@gmail.com <MichaelQTLam@gmail.com@7476bfce-e641-1f32-b5ea-30df2487683e>
2013-11-26 23:53:50 +0000

    Added code to convert to mesh.

--- edu.osu.graphics.project5/src/SimpleProgram.cpp
+++ edu.osu.graphics.project5/src/SimpleProgram.cpp
@@ -31,12 +31,22 @@ typedef Angel::vec4 point4;
 
 int NumVertices = 0; //(6 faces)(2 triangles/face)(3 vertices/triangle)
 
-GLuint model_view;  // model-view matrix uniform shader variable location
+GLuint model; // model matrix uniform shader variable location
+GLuint view;  // view matrix uniform shader variable location
 GLuint projection; // projection matrix uniform shader variable location
 
 //----------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------
+struct ImageMesh {
+	int numTrianglePoints;
+	vec4* vertices;
+	vec3* colors;
+};
+
+// stores all image meshes
+ImageMesh imageMesh;
+
 struct ImageChannel {
 	int width;
 	int height;
@@ -49,73 +59,74 @@ struct ImageChannel {
 ImageChannel imageChannel;
 // OpenGL initialization
 void init() {
+	imageMesh.numTrianglePoints = 0;
 
 	//
 	imageChannel.width = -1;
 	imageChannel.height = -1;
 
-	// Create a vertex array object
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	//// Create a vertex array object
+	//GLuint vao;
+	//glGenVertexArrays(1, &vao);
+	//glBindVertexArray(vao);
 
-	// Create and initialize a buffer object
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer( GL_ARRAY_BUFFER, buffer);
-	//Must be modified
-	//	glBufferData();
+	//// Create and initialize a buffer object
+	//GLuint buffer;
+	//glGenBuffers(1, &buffer);
+	//glBindBuffer( GL_ARRAY_BUFFER, buffer);
+	////Must be modified
+	////	glBufferData();
 
-	// Load shaders and use the resulting shader program
-	GLuint program = InitShader("shaders/vshader.glsl", "shaders/fshader.glsl");
-	glUseProgram(program);
+	//// Load shaders and use the resulting shader program
+	//GLuint program = InitShader("shaders/vshader32.glsl", "shaders/fshader32.glsl");
+	//glUseProgram(program);
 
 	// set up vertex arrays
 
-	// Initialize shader lighting parameters
-	// RAM: No need to change these...we'll learn about the details when we
-	// cover Illumination and Shading
-	point4 light_position(1.5, 0.5, 2.0, 1.0);
-	color4 light_ambient(0.2, 0.2, 0.2, 1.0);
-	color4 light_diffuse(1.0, 1.0, 1.0, 1.0);
-	color4 light_specular(1.0, 1.0, 1.0, 1.0);
+	//// Initialize shader lighting parameters
+	//// RAM: No need to change these...we'll learn about the details when we
+	//// cover Illumination and Shading
+	//point4 light_position(1.5, 0.5, 2.0, 1.0);
+	//color4 light_ambient(0.2, 0.2, 0.2, 1.0);
+	//color4 light_diffuse(1.0, 1.0, 1.0, 1.0);
+	//color4 light_specular(1.0, 1.0, 1.0, 1.0);
 
-	color4 material_ambient(1.0, 0.0, 1.0, 1.0);
-	color4 material_diffuse(1.0, 0.8, 0.0, 1.0);
-	color4 material_specular(1.0, 0.8, 0.0, 1.0);
-	float material_shininess = 100.0;
+	//color4 material_ambient(1.0, 0.0, 1.0, 1.0);
+	//color4 material_diffuse(1.0, 0.8, 0.0, 1.0);
+	//color4 material_specular(1.0, 0.8, 0.0, 1.0);
+	//float material_shininess = 100.0;
 
-	color4 ambient_product = light_ambient * material_ambient;
-	color4 diffuse_product = light_diffuse * material_diffuse;
-	color4 specular_product = light_specular * material_specular;
+	//color4 ambient_product = light_ambient * material_ambient;
+	//color4 diffuse_product = light_diffuse * material_diffuse;
+	//color4 specular_product = light_specular * material_specular;
 
-	glUniform4fv( glGetUniformLocation(program, "AmbientProduct"), 1,
-			ambient_product);
-	glUniform4fv( glGetUniformLocation(program, "DiffuseProduct"), 1,
-			diffuse_product);
-	glUniform4fv( glGetUniformLocation(program, "SpecularProduct"), 1,
-			specular_product);
+	//glUniform4fv( glGetUniformLocation(program, "AmbientProduct"), 1,
+	//		ambient_product);
+	//glUniform4fv( glGetUniformLocation(program, "DiffuseProduct"), 1,
+	//		diffuse_product);
+	//glUniform4fv( glGetUniformLocation(program, "SpecularProduct"), 1,
+	//		specular_product);
 
-	glUniform4fv( glGetUniformLocation(program, "LightPosition"), 1,
-			light_position);
+	//glUniform4fv( glGetUniformLocation(program, "LightPosition"), 1,
+	//		light_position);
 
-	glUniform1f( glGetUniformLocation(program, "Shininess"),
-			material_shininess);
+	//glUniform1f( glGetUniformLocation(program, "Shininess"),
+	//		material_shininess);
 
-	model_view = glGetUniformLocation(program, "ModelView");
-	projection = glGetUniformLocation(program, "Projection");
-	mat4 p;
+	//model_view = glGetUniformLocation(program, "ModelView");
+	//projection = glGetUniformLocation(program, "Projection");
+	//mat4 p;
 
-	///////// Should be modified
-	point4 eye(0, 0, 0, 1.0);
-	point4 at(0, 0, 0, 1.0);
-	vec4 up(0, 0, 1, 0.0);
+	/////////// Should be modified
+	//point4 eye(0, 0, 0, 1.0);
+	//point4 at(0, 0, 0, 1.0);
+	//vec4 up(0, 0, 1, 0.0);
 
-	mat4 mv = LookAt(eye, at, up);
-	//vec4 v = vec4(0.0, 0.0, 1.0, 1.0);
+	//mat4 mv = LookAt(eye, at, up);
+	////vec4 v = vec4(0.0, 0.0, 1.0, 1.0);
 
-	glUniformMatrix4fv(model_view, 1, GL_TRUE, mv);
-	glUniformMatrix4fv(projection, 1, GL_TRUE, p);
+	//glUniformMatrix4fv(model_view, 1, GL_TRUE, mv);
+	//glUniformMatrix4fv(projection, 1, GL_TRUE, p);
 
 	glEnable( GL_DEPTH_TEST);
 	glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -125,7 +136,9 @@ void init() {
 
 void display(void) {
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glDrawArrays( GL_TRIANGLES, 0, NumVertices);
+	//glDrawArrays( GL_TRIANGLES, 0, NumVertices);
+	glDrawArrays(GL_TRIANGLES, 0, imageMesh.numTrianglePoints);
+	glFlush();
 	glutSwapBuffers();
 }
 
@@ -151,6 +164,139 @@ void keyboard(unsigned char key, int x, int y) {
  */
 
 /* glut.h includes gl.h and glu.h*/
+
+void drawImage() {
+	const int nCols = imageChannel.width;
+	const int nRows = imageChannel.height;
+
+	// determine the horizontal and vertical measure of each unit square to draw
+	float horzUnit = 2.0 / (nCols);
+	float vertUnit = 2.0 / (nRows);
+	
+	imageMesh.numTrianglePoints = 6*nRows*nCols;
+	imageMesh.vertices = new vec4[imageMesh.numTrianglePoints];
+	imageMesh.colors = new vec3[imageMesh.numTrianglePoints];
+
+	for (int row = 0; row < nRows; row++) {
+		for (int col = 0; col < nCols; col++) {
+			int index = 6 * (row * nCols + col);
+
+			// make a grid square:
+			// draw two unit triangles based on our defined units: horzUnit, vertUnit
+			// first draw relative to origin, then subtract 1 in both coordinates to put at (-1, -1)
+
+			float depth = 1.0*imageChannel.gray[row][col]/255;
+
+			// first triangle
+			imageMesh.vertices[index] = vec4(col * horzUnit - 1, -row * vertUnit + 1, depth, 1);
+			imageMesh.vertices[index + 1] = vec4(col * horzUnit - 1,
+					-(row+1) * vertUnit + 1, depth, 1);
+			imageMesh.vertices[index + 2] = vec4((col + 1) * horzUnit - 1,
+					-row * vertUnit + 1, depth, 1);
+			// second triangle
+			imageMesh.vertices[index + 3] = vec4((col + 1) * horzUnit - 1, -row * vertUnit + 1, depth, 1);
+			imageMesh.vertices[index + 4] = vec4((col + 1) * horzUnit - 1,
+					-(row + 1) * vertUnit + 1, depth, 1);
+			imageMesh.vertices[index + 5] = vec4(col * horzUnit - 1,
+					-(row + 1) * vertUnit + 1, depth, 1);
+
+			// assign appropriate color:
+			float rgb[3] = { 1.0*imageChannel.r[row][col]/255, 1.0*imageChannel.g[row][col]/255, 1.0*imageChannel.b[row][col]/255 };
+			//float rgb[3] = {1,0,0};
+			for (int i = 0; i < 6; i++)
+				imageMesh.colors[index + i] = vec3(rgb[0], rgb[1], rgb[2]);
+		}
+	}
+
+	// Create a vertex array object---OpenGL needs this to manage the Vertex
+	// Buffer Object
+	GLuint vao[1];
+
+	// Generate the vertex array and then bind it to make make it active.
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+
+	// Create and initialize a buffer object---that's the memory buffer that
+	// will be on the card!
+	GLuint buffer[2];
+
+	// We only need one for this example.
+	glGenBuffers(2, buffer);
+
+	// Bind makes it the active VBO
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+
+	// Here we copy the vertex data into our buffer on the card.  The parameters
+	// tell it the type of buffer object, the size of the data in bytes, the
+	// pointer for the data itself, and a hint for how we intend to use it.
+	glBufferData(GL_ARRAY_BUFFER,
+			(imageMesh.numTrianglePoints) * sizeof(vec4), imageMesh.vertices,
+			GL_STATIC_DRAW);
+
+	// Load the shaders.  Note that this function is not offered by OpenGL
+	// directly, but provided as a convenience.
+	GLuint program = InitShader("shaders/vshader32.glsl", "shaders/fshader32.glsl");
+	glUseProgram(program);
+
+	// Initialize the vertex position attribute from the vertex shader.  When
+	// the shader and the program are linked, a table is created for the shader
+	// variables.  Here, we get the index of the vPosition variable.
+	GLuint loc = glGetAttribLocation(program, "vPosition");
+
+	// We want to set this with an array!
+	glEnableVertexAttribArray(loc);
+
+	// We map it to this offset in our current buffer (VBO) So, our position
+	// data is going into loc and contains 2 floats.  The parameters to this
+	// function are the index for the shader variable, the number of components,
+	// the type of the data, a boolean for whether or not this data is
+	// normalized (0--1), stride (or byte offset between consective attributes),
+	// and a pointer to the first component.  Note that BUFFER_OFFSET is a macro
+	// defined in the Angel.h file.
+	glVertexAttribPointer(loc, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+	// Now repeat lots of that stuff for the colors
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);
+	glBufferData(GL_ARRAY_BUFFER, (imageMesh.numTrianglePoints) * sizeof(vec3), imageMesh.colors,
+	GL_STATIC_DRAW);
+
+	GLuint colorLoc = glGetAttribLocation(program, "vColor");
+	glEnableVertexAttribArray(colorLoc);
+	glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+	// Make the background white
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	// Retrieve transformation uniform variable locations
+	//model_view = glGetUniformLocation(program, "ModelView");
+	model = glGetUniformLocation(program, "Model");
+	view = glGetUniformLocation(program, "View");
+	projection = glGetUniformLocation(program, "Projection");
+
+	//Set up model matrix
+	mat4 m; // identity
+	glUniformMatrix4fv(model, 1, GL_TRUE, m);
+
+	//Set up the view matrix with LookAt
+	point4 eye(0, 0, 5, 1.0);
+	point4 at(0, 0, 0, 1.0);
+	vec4 up(0, 1, 0, 0.0);
+
+	mat4 v = LookAt(eye, at, up);
+	glUniformMatrix4fv(view, 1, GL_TRUE, v);
+
+	//Setup the view volume
+	//mat4 proj = Perspective(45, 1, 0.1, 20);
+	mat4 proj = Ortho(-1, 1, -1, 1, 0.1, 20);
+
+	glUniformMatrix4fv(projection, 1, GL_TRUE, proj);
+
+	// z buffer stuff
+	glEnable( GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	cout << "Image Initialized." << endl;
+}
 
 /**
  * Data Structure
@@ -188,6 +334,8 @@ void loadImage(const char* fileName) {
 		}
 	}
 	cout << "Load successfully." << endl;
+
+	drawImage();
 }
 enum MENU_ITEMS {
 	load, rChannel, gChannel, bChannel, grayChannel
